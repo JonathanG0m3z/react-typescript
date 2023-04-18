b8bfa70c379228a641121a91119d032a1daee9d1
JonathanG0m3z <104044651+JonathanG0m3z@users.noreply.github.com>
2023-04-18 13:14:16 -0500

    Creation hook useAuth
    -npm install
    -js-cookie added
    -useAuth incomplete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { RandomFox } from "@/components/RandomFox";
+import { MouseEventHandler, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+const generateId = (): number => new Date().getTime(); 
 
-export default function Home() {
+type ImageItem = {id: number, url: string};
+
+export default function Home(): JSX.Element {
+  const [images, setimages] = useState<Array<ImageItem>>([]);
+
+  const handleButton: MouseEventHandler<HTMLButtonElement> = (event)=>{
+    event.preventDefault();
+    setimages([...images, {id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg`}]);
+  };
+  
   return (
     <div>
       <main>
-        <h1>Hello platzi</h1>
+        <h1 className="text-green-500">
+          Hello platzi
+        </h1>
+        <button onClick={handleButton}>
+          Add new fox
+        </button>
+        {images.map((item)=>
+        <div key={item.id} className="p-4">
+          <RandomFox image={item.url} />
+        </div>
+        )}
       </main>
     </div>
   )
